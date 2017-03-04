52e6d85aa5074a332798333428924f6843b05f42
kyuusaku <q.suo@foxmail.com>
2017-03-04 20:10:10 +0800

    add cifar10 resnet 20 experiment

---
+++ scripts/cifar10-resnet-20-seed1.sh
@@ -0,0 +1,10 @@
+export CUDA_VISIBLE_DEVICES=2,3
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/nccl/build/lib
+
+set -x
+set -e
+export PYTHONUNBUFFERED="True"
+
+LOG=./log/cifar10/resnet-20-seed1.log
+th main.lua -dataset cifar10 -nGPU 2 -batchSize 128 -netType resnet -nEpochs 200 -depth 20 -manualSeed 1 \
+2>&1 | tee -i $LOG
