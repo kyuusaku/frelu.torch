2789a5165fb31d7f9a0d5279104d6b5e2444ecc6
kyuusaku <q.suo@foxmail.com>
2017-04-22 13:41:39 +0800

    add inplace & test for MyAdd

--- models/glu/MyAdd.lua
+++ models/glu/MyAdd.lua
@@ -1,10 +1,11 @@
 local MyAdd, parent = torch.class('nn.MyAdd', 'nn.Module')
 
-function MyAdd:__init(val)
+function MyAdd:__init(val, inplace)
    parent.__init(self)
   
    self.bias = torch.Tensor(1)
    self.gradBias = torch.Tensor(1)
+   self.inplace = inplace or false
 
    self:reset(val)
 end
@@ -14,14 +15,22 @@ function MyAdd:reset(val)
 end
 
 function MyAdd:updateOutput(input)
-   self.output:resizeAs(input):copy(input)
+   if self.inplace then
+      self.output = input
+   else
+      self.output:resizeAs(input):copy(input)
+   end
    self.output:add(self.bias[1]);
    return self.output
 end
 
 function MyAdd:updateGradInput(input, gradOutput)
    if self.gradInput then
-      self.gradInput:resizeAs(gradOutput):copy(gradOutput) 
+      if self.inplace then
+         self.gradInput = gradOutput
+      else
+         self.gradInput:resizeAs(gradOutput):copy(gradOutput)
+      end 
       return self.gradInput
    end
 end
