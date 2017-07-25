3736070e01babf9d8f91796cbf0ecf3a5a67d073
Suo <q.suo@foxmail.com>
2017-07-25 15:25:12 +0800

    add nin-relu

---
+++ run-relu.sh
@@ -0,0 +1,2 @@
+# 2017-07-25
+bash scripts/plain-seed.sh cifar10 nin-relu 0 1 4
---
+++ scripts/plain-seed.sh
@@ -0,0 +1,10 @@
+export CUDA_VISIBLE_DEVICES=$5
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/nccl/build/lib
+
+set -x
+set -e
+export PYTHONUNBUFFERED="True"
+
+LOG=./log/$1/$2-seed$3.log
+th main.lua -dataset $1 -nGPU $4 -batchSize 128 -netType $2 -nEpochs 200 -manualSeed $3 \
+2>&1 | tee -i $LOG
