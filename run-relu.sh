5bcf602997311e6e958e95e3faf49eaf26ea739b
Suo <q.suo@foxmail.com>
2017-07-26 21:07:55 +0800

    update run relu

--- run-relu.sh
+++ run-relu.sh
@@ -2,8 +2,8 @@
 #bash scripts/plain-seed.sh cifar10 nin-relu-bn 0 1 4
 #bash scripts/plain-seed.sh cifar10 nin-relu-bn 1 1 4
 #bash scripts/plain-seed.sh cifar10 nin-relu-bn 2 1 4
-bash scripts/plain-seed.sh cifar10 nin-relu-bn 3 1 4
-bash scripts/plain-seed.sh cifar10 nin-relu-bn 4 1 4
+#bash scripts/plain-seed.sh cifar10 nin-relu-bn 3 1 4
+#bash scripts/plain-seed.sh cifar10 nin-relu-bn 4 1 4
 
 bash scripts/plain-seed.sh cifar100 nin-relu-bn 0 1 4
 bash scripts/plain-seed.sh cifar100 nin-relu-bn 1 1 4
