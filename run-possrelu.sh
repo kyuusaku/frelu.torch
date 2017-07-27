dbd4327a510b6e0d35e54b896bab925ff6f39d2a
Suo <q.suo@foxmail.com>
2017-07-28 00:22:11 +0800

    add nin-possrelu-bn

--- run-possrelu.sh
+++ run-possrelu.sh
@@ -1,30 +1,12 @@
-# 2017-04-28
-#bash scripts/cifar100-elu-resnet-possrelu-seed.sh 20 0 6,7
-#bash scripts/cifar100-elu-resnet-possrelu-seed.sh 20 1 6,7
-#bash scripts/cifar100-elu-resnet-possrelu-seed.sh 20 2 6,7
-#bash scripts/cifar100-elu-resnet-possrelu-seed.sh 20 3 6,7
-#bash scripts/cifar100-elu-resnet-possrelu-seed.sh 20 4 6,7
+# 2017-07-25
+bash scripts/plain-seed.sh cifar10 nin-possrelu-bn 0 1 5
+bash scripts/plain-seed.sh cifar10 nin-possrelu-bn 1 1 5
+bash scripts/plain-seed.sh cifar10 nin-possrelu-bn 2 1 5
+bash scripts/plain-seed.sh cifar10 nin-possrelu-bn 3 1 5
+bash scripts/plain-seed.sh cifar10 nin-possrelu-bn 4 1 5
 
-#bash scripts/cifar100-elu-resnet-possrelu-seed.sh 32 0 6,7
-#bash scripts/cifar100-elu-resnet-possrelu-seed.sh 32 1 6,7
-#bash scripts/cifar100-elu-resnet-possrelu-seed.sh 32 2 6,7
-#bash scripts/cifar100-elu-resnet-possrelu-seed.sh 32 3 6,7
-#bash scripts/cifar100-elu-resnet-possrelu-seed.sh 32 4 6,7
-
-#bash scripts/cifar100-elu-resnet-possrelu-seed.sh 44 0 6,7
-#bash scripts/cifar100-elu-resnet-possrelu-seed.sh 44 1 6,7
-#bash scripts/cifar100-elu-resnet-possrelu-seed.sh 44 2 6,7
-#bash scripts/cifar100-elu-resnet-possrelu-seed.sh 44 3 6,7
-#bash scripts/cifar100-elu-resnet-possrelu-seed.sh 44 4 6,7
-
-#bash scripts/cifar100-elu-resnet-possrelu-seed.sh 56 0 6,7
-bash scripts/cifar100-elu-resnet-possrelu-seed.sh 56 1 6,7
-#bash scripts/cifar100-elu-resnet-possrelu-seed.sh 56 2 6,7
-#bash scripts/cifar100-elu-resnet-possrelu-seed.sh 56 3 6,7
-#bash scripts/cifar100-elu-resnet-possrelu-seed.sh 56 4 6,7
-
-bash scripts/cifar100-elu-resnet-possrelu-seed.sh 110 0 6,7
-#bash scripts/cifar100-elu-resnet-possrelu-seed.sh 110 1 6,7
-#bash scripts/cifar100-elu-resnet-possrelu-seed.sh 110 2 6,7
-#bash scripts/cifar100-elu-resnet-possrelu-seed.sh 110 3 6,7
-#bash scripts/cifar100-elu-resnet-possrelu-seed.sh 110 4 6,7
+bash scripts/plain-seed.sh cifar100 nin-possrelu-bn 0 1 5
+bash scripts/plain-seed.sh cifar100 nin-possrelu-bn 1 1 5
+bash scripts/plain-seed.sh cifar100 nin-possrelu-bn 2 1 5
+bash scripts/plain-seed.sh cifar100 nin-possrelu-bn 3 1 5
+bash scripts/plain-seed.sh cifar100 nin-possrelu-bn 4 1 5
