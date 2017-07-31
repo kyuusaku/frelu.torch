d360e03d8101e4805556b3411b68118b002208be
Suo <q.suo@foxmail.com>
2017-08-01 06:33:59 +0800

    fix bug

--- models/init.lua
+++ models/init.lua
@@ -24,6 +24,7 @@ function M.setup(opt, checkpoint)
       local modelPath = paths.concat(opt.resume, checkpoint.modelFile)
       assert(paths.filep(modelPath), 'Saved model not found: ' .. modelPath)
       print('=> Resuming model from ' .. modelPath)
+      require './models/glu/MyAdd'
       model = torch.load(modelPath):cuda()
    elseif opt.retrain ~= 'none' then
       assert(paths.filep(opt.retrain), 'File not found: ' .. opt.retrain)
