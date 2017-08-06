a3632308cf2b02c34d2ea5fc4b1e574c3f66fa2e
Suo <q.suo@foxmail.com>
2017-08-06 14:00:29 +0800

    add show function to see bias value of MyAdd

---
+++ show.lua
@@ -0,0 +1,31 @@
+require 'torch'
+require 'nn'
+require 'cunn'
+require 'cudnn'
+PosSReLU = require './glu/PosSReLU'
+
+
+
+local function parse(arg)
+	local cmd = torch.CmdLine()
+	cmd:option('-model',      'none',   'Path to model to show')
+	local opt = cmd:parse(arg or {})
+	return opt
+end
+
+local function show(model, name)   
+    for k,v in pairs(model:findModules(name)) do
+        if v.weight ~= nil then
+            print('weight:', v.weight)
+        end
+        if v.bias ~= nil then
+            print('bias:', v.bias)
+        end
+    end
+end
+
+local opt = opts.parse(arg)
+
+local model = torch.load(opt.model)
+
+show(model,'nn.MyAdd')
