16c4a90b69e9f99f51178403965c4a00ef79c6ff
Suo <q.suo@foxmail.com>
2017-09-02 21:08:44 +0800

    prelu 0.1 nan

--- run-prelu.sh
+++ run-prelu.sh
@@ -1,6 +1,6 @@
 # 2017-07-29
-bash scripts/plain-seed.sh cifar100 pelu-smallnet-prelu 0 1 $1
-bash scripts/plain-seed.sh cifar100 pelu-smallnet-prelu 1 1 $1
-bash scripts/plain-seed.sh cifar100 pelu-smallnet-prelu 2 1 $1
-bash scripts/plain-seed.sh cifar100 pelu-smallnet-prelu 3 1 $1
-bash scripts/plain-seed.sh cifar100 pelu-smallnet-prelu 4 1 $1
+bash scripts/plain-seed.sh cifar100 pelu-smallnet-prelu 0 1 $1 0.01
+bash scripts/plain-seed.sh cifar100 pelu-smallnet-prelu 1 1 $1 0.01
+bash scripts/plain-seed.sh cifar100 pelu-smallnet-prelu 2 1 $1 0.01
+bash scripts/plain-seed.sh cifar100 pelu-smallnet-prelu 3 1 $1 0.01
+bash scripts/plain-seed.sh cifar100 pelu-smallnet-prelu 4 1 $1 0.01
