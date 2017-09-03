971754d1fbf558c4153d4de6cdaa118c42d94241
Suo <q.suo@foxmail.com>
2017-09-03 15:48:09 +0800

    add prelu bn exp

--- run-prelu.sh
+++ run-prelu.sh
@@ -1,6 +1,12 @@
 # 2017-07-29
-bash scripts/plain-seed.sh cifar100 pelu-smallnet-prelu 0 1 $1 0.01
-bash scripts/plain-seed.sh cifar100 pelu-smallnet-prelu 1 1 $1 0.01
-bash scripts/plain-seed.sh cifar100 pelu-smallnet-prelu 2 1 $1 0.01
-bash scripts/plain-seed.sh cifar100 pelu-smallnet-prelu 3 1 $1 0.01
-bash scripts/plain-seed.sh cifar100 pelu-smallnet-prelu 4 1 $1 0.01
+bash scripts/plain-seed.sh cifar100 pelu-smallnet-prelu-bn 0 1 $1 0.01
+bash scripts/plain-seed.sh cifar100 pelu-smallnet-prelu-bn 1 1 $1 0.01
+bash scripts/plain-seed.sh cifar100 pelu-smallnet-prelu-bn 2 1 $1 0.01
+bash scripts/plain-seed.sh cifar100 pelu-smallnet-prelu-bn 3 1 $1 0.01
+bash scripts/plain-seed.sh cifar100 pelu-smallnet-prelu-bn 4 1 $1 0.01
+
+bash scripts/plain-seed.sh cifar100 pelu-smallnet-prelu-bn 0 1 $1 0.1
+bash scripts/plain-seed.sh cifar100 pelu-smallnet-prelu-bn 1 1 $1 0.1
+bash scripts/plain-seed.sh cifar100 pelu-smallnet-prelu-bn 2 1 $1 0.1
+bash scripts/plain-seed.sh cifar100 pelu-smallnet-prelu-bn 3 1 $1 0.1
+bash scripts/plain-seed.sh cifar100 pelu-smallnet-prelu-bn 4 1 $1 0.1
