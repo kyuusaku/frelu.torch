9a2ae1180c3bc4fdf686f4d339a7135c7c54741b
Suo <q.suo@foxmail.com>
2017-12-05 20:59:25 +0800

    add srelu exp

---
+++ scripts/cifar100-pelu-smallnet-srelu-seed.sh
@@ -0,0 +1,15 @@
+export CUDA_VISIBLE_DEVICES=$1
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/nccl/build/lib
+
+set -x
+set -e
+export PYTHONUNBUFFERED="True"
+
+for (( i=0; i<5; i++ ))  
+do  
+LOG=./log/cifar100/pelu-smallnet-srelu-seed$i.log
+th main.lua -dataset cifar100 -nGPU 1 -batchSize 128 -netType pelu-smallnet-srelu -nEpochs 200 -LR 0.01 -manualSeed $i \
+2>&1 | tee -i $LOG
+done
+
+
