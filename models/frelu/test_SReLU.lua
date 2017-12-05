ec402926302d02e2b41795a008c0d4ff80e553ec
Suo <q.suo@foxmail.com>
2017-12-05 20:41:13 +0800

    add jac test for srelu

--- models/frelu/test_SReLU.lua
+++ models/frelu/test_SReLU.lua
@@ -15,3 +15,54 @@ gnuplot.grid(true)
 --print(ii)
 --print(oo)
 --print(gi)
+
+local mytest = torch.TestSuite()
+
+local tester = torch.Tester()
+
+local jac = nn.Jacobian
+local precision = 1e-5
+
+function mytest.testSReLU()
+    
+    local input = torch.rand(1, 10)
+    local model = ShiftedReLU(-1,true,false)
+    local err = jac.testJacobian(model, input)
+    tester:assert(err < precision, 'SReLU(true) Linear gradient')
+
+    input = torch.rand(5, 10, 6)
+    model = ShiftedReLU(-1,true,false)
+    err = jac.testJacobian(model, input)
+    tester:assert(err < precision, 'SReLU(true) Map gradient')
+    
+    input = torch.rand(5, 10)
+    model = nn.Sequential()
+    model:add(nn.Linear(10, 10))
+    model:add(ShiftedReLU(-1,true,false))
+    model:add(nn.LogSoftMax())
+    err = jac.testJacobian(model, input)
+    tester:assert(err < precision, 'SReLU(true) with other layers')
+
+    input = torch.rand(1, 10)
+    model = ShiftedReLU(-1,false,false)
+    err = jac.testJacobian(model, input)
+    tester:assert(err < precision, 'SReLU(false) Linear gradient')
+
+    input = torch.rand(5, 10, 6)
+    model = ShiftedReLU(-1,false,false)
+    err = jac.testJacobian(model, input)
+    tester:assert(err < precision, 'SReLU(false) Map gradient')
+    
+    input = torch.rand(5, 10)
+    model = nn.Sequential()
+    model:add(nn.Linear(10, 10))
+    model:add(ShiftedReLU(-1,false,false))
+    model:add(nn.LogSoftMax())
+    err = jac.testJacobian(model, input)
+    tester:assert(err < precision, 'SReLU(false) with other layers')
+
+end
+
+
+tester:add(mytest)
+tester:run()
