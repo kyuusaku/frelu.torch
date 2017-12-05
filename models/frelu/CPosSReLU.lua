455bee4e041bcd5bef1e7f3247cea4f10fe59698
Suo <q.suo@foxmail.com>
2017-12-05 15:48:14 +0800

    clean code

--- models/frelu/CPosSReLU.lua
+++
@@ -1,9 +0,0 @@
-require 'nn'
-
-function CPosSReLU(val)
-	return nn.Sequential()
-		:add(nn.ReLU(true))
-		:add(nn.AddConstant(val))
-end
-
-return CPosSReLU
--- models/frelu/WELUVerify.lua
+++
@@ -1,8 +0,0 @@
-require 'nn'
-WELU=require 'WELU'
-
-data = torch.randn(128,256,16,16)
-mlp = nn.Sequential()
-mlp:add(WELU(5,256))
-print(mlp)
-print(#mlp:forward(data))
