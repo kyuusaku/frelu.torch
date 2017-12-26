4c0aef9443f18c1b68c574c1d68b2edbbc35ec4f
Suo <q.suo@foxmail.com>
2017-12-26 22:23:28 +0800

    update

--- scripts/imagenet-resnet.sh
+++ scripts/imagenet-resnet.sh
@@ -6,5 +6,5 @@ set -e
 export PYTHONUNBUFFERED="True"
 
 LOG=./log/imagenet/$1-$2.log
-th main.lua -netType $1 -depth $2 -nGPU $3 -nThreads $5 -batchSize 256 -data /home/lab/Dataset/ImageNet/ -save checkpoints-imagenet \
+th main.lua -netType $1 -depth $2 -nGPU $3 -nThreads $5 -batchSize 256 -data /home/lab/Dataset/ImageNet/ -save checkpoints-imagenet-$1 \
 2>&1 | tee -i $LOG
