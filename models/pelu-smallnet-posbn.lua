9044ac082b3fc942526c08e58752557a9b59f318
Suo <q.suo@foxmail.com>
2018-01-14 18:56:52 +0800

    update posbn

--- models/pelu-smallnet-posbn.lua
+++ models/pelu-smallnet-posbn.lua
@@ -32,12 +32,7 @@ local function createModel(opt)
       for k,v in pairs(model:findModules(name)) do
          local n = v.kW*v.kH*v.nOutputPlane
          v.weight:normal(0,math.sqrt(2/n))
-         if cudnn.version >= 4000 then
-            v.bias = nil
-            v.gradBias = nil
-         else
-            v.bias:zero()
-         end
+         v.bias:zero()
       end
    end
    local function BNInit(name)
